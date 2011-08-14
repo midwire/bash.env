76ccf2893901a9b265cda6beab1543552f5ccc0d
Midwire <chris@midwiretech.com>
2011-08-14 14:19:49 -0500

    Removed fidelity specific stuff from the master branch

--- fidelity.sh
+++
@@ -1,8 +0,0 @@
-# Make sure you have your ssh key in the remote ~/.ssh/authorized_keys file
-function propagate_environment_to_fidelity_servers {
-	servers="amd-203 amd-230 amd-215 amd-241 pr-ra-1 pr-ra-2 pr-ra-3 pr-ra-4 ub40"
-	for s in $servers; do
-		echo $s
-		cat ~/.env/fidelity/.blackburn-environment | ssh $s "cat > .blackburn-environment"
-	done
-}
