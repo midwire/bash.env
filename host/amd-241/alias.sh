feb900c05074348b726824845f0041d10d90ea91
Midwire <chris@midwiretech.com>
2011-08-26 18:51:24 -0500

    Got rid of the tput stuff because it doesn't work on certain versions of Solaris Added more detail to the customization section of the README Added a `.gitignore` file to the `host` directory and removed my own customized host files

--- host/amd-241/alias.sh
+++
@@ -1,4 +0,0 @@
-alias logs='cd /var/log/pinkbox/production'
-alias mgr='cd $HOME/production/apps/pinkbox/management; RAILS_ENV=production ./bin/console'
-alias root='cd $HOME/production/apps/pinkbox'
-alias ps='/usr/ucb/ps'
