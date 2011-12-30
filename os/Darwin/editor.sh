395390bfe7f64554b56f2eae3617a4cbcc3dd969
Chris Blackburn <chris@midwiretech.com>
2011-12-30 14:19:52 -0600

    Renamed textmate.sh to editor.sh

---
+++ os/Darwin/editor.sh
@@ -0,0 +1,16 @@
+# Delete this file if you don't use TextMate
+# Textmate Editing and Sourcing aliases
+export EDITOR="$HOME/bin/mate"
+alias e="$EDITOR ."
+alias ea="$EDITOR $DOT_ENV_PATH/host/$HOSTNAME/alias.sh"
+alias eas=". $DOT_ENV_PATH/host/$HOSTNAME/alias.sh"
+alias eb="$EDITOR $HOME/bin"
+alias ee="$EDITOR $DOT_ENV_PATH $HOME/.bashrc"
+alias ees=". $DOT_ENV_PATH/source.sh"
+alias ef="$EDITOR $DOT_ENV_PATH/host/$HOSTNAME/functions.sh"
+alias efs=". $DOT_ENV_PATH/host/$HOSTNAME/functions.sh"
+alias eh="$EDITOR $HOME/.ssh"
+alias ep="$EDITOR $DOT_ENV_PATH/host/$HOSTNAME/path.sh"
+alias eps=". $DOT_ENV_PATH/host/$HOSTNAME/path.sh"
+alias m="$EDITOR"
+alias vi="$EDITOR"
