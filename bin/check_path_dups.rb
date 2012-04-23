caca5fce833922ea5224046668067f1d23416801
Chris Blackburn <chris@midwiretech.com>
2012-04-23 09:30:02 -0500

    Added this script to check path duplicates.

---
+++ bin/check_path_dups.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# check path duplicates
+
+uniq_entries = []
+ENV['PATH'].split(/:/).each do |entry|
+  unless uniq_entries.include? entry
+    uniq_entries << entry
+  else
+    puts ">>> Duplicate: [#{entry}]"
+  end
+end
