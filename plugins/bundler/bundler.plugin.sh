f30c07eeb0cd729bb1466b1ce2e3fb12606edf08
Chris Blackburn <chris@midwiretech.com>
2013-05-11 20:33:22 -0500

    Added some bundler aliases

--- plugins/bundler/bundler.plugin.sh
+++ plugins/bundler/bundler.plugin.sh
@@ -1,6 +1,10 @@
 THISDIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Bundle Exec aliases
 alias be="bundle exec"
+alias bers="bundle exec rails s"
+alias berc="bundle exec rails c"
+
 alias bi="bundle install --path vendor"
 alias bil="bi --local"
 alias binit="bi && bundle package && echo 'vendor/ruby' >> .gitignore"
