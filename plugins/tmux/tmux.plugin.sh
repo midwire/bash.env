b920b35cf90d2e6995b9dbf33b8e70100bf57665
Chris Blackburn <chris@midwiretech.com>
2013-06-20 12:38:15 -0500

    added some aliases to the tmux plugin

--- plugins/tmux/tmux.plugin.sh
+++ plugins/tmux/tmux.plugin.sh
@@ -5,3 +5,7 @@ alias tml='tmux list-sessions'
 alias tmw='tmux attach -t work || tmux new -s work'
 alias tms='tmux attach -t servers || tmux new -s servers'
 alias tmd='tmux attach -t dotfile || tmux new -s dotfile'
+alias tmks='tmux kill-session'
+alias etmx='edit ~/.tmux.conf'
+
+PS1="$PS1"'$([ -n "$TMUX" ] && tmux setenv TMUXPWD_$(tmux display -p "#D" | tr -d %) "$PWD")'
