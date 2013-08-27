258f9fe68f0844c8286209aed0ba68065fa3f4eb
Chris Blackburn <chris@midwiretech.com>
2013-08-27 08:32:02 -0500

    better tmux aliases

--- plugins/tmux/tmux.plugin.sh
+++ plugins/tmux/tmux.plugin.sh
@@ -2,10 +2,8 @@ THISDIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # aliases
 alias tml='tmux list-sessions'
-alias tmw='tmux attach -t work || tmux new -s work'
-alias tms='tmux attach -t servers || tmux new -s servers'
-alias tmd='tmux attach -t dotfile || tmux new -s dotfile'
 alias tmks='tmux kill-session'
 alias etmx='edit ~/.tmux.conf'
+alias tma='alias|grep tmux'
 
 PS1="$PS1"'$([ -n "$TMUX" ] && tmux setenv TMUXPWD_$(tmux display -p "#D" | tr -d %) "$PWD")'
