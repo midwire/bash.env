8b49665b72a4719a14ae6b2383ee2da543f1b8e5
Chris Blackburn <cblackburn@ajla.net>
2020-01-08 09:46:54 -0700

    Add the Ansible plugin

---
+++ plugins/ansible/ansible.plugin.sh
@@ -0,0 +1,15 @@
+THISDIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Ansible-related aliases
+alias acfg='ansible-config'
+alias acon='ansible-console'
+alias adoc='ansible-doc'
+alias agal='ansible-galaxy'
+alias ainv='ansible-inventory'
+alias ans='ansible'
+alias ap='ansible-playbook'
+alias aplaybook='ansible-playbook'
+alias apull='ansible-pull'
+alias arinit='ansible-role-init'
+alias av='ansible-vault'
+alias aver='ansible --version'
