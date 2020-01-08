33d70439205ceb4b5bfb55b081973cb9c1f3e5fa
Chris Blackburn <cblackburn@ajla.net>
2020-01-08 09:52:46 -0700

    Add the Homebrew plugin

---
+++ plugins/brew/brew.plugin.sh
@@ -0,0 +1,10 @@
+# A plaguerized version of the ZSH plugin: https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/brew/brew.plugin.zsh
+
+alias brewp='brew pin'
+alias brews='brew list -1'
+alias brewsp='brew list --pinned'
+alias bubo='brew update && brew outdated'
+alias bubc='brew upgrade && brew cleanup'
+alias bubu='bubo && bubc'
+alias bcubo='brew update && brew cask outdated'
+alias bcubc='brew cask reinstall $(brew cask outdated) && brew cleanup'
