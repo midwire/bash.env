7388a4980084e967dbf010b9297c26f58737c61f
Chris Blackburn <cblackburn@ajla.net>
2020-01-08 09:29:10 -0700

    Add psgrep plugin

---
+++ plugins/psgrep/psgrep.plugin.sh
@@ -0,0 +1,8 @@
+# This plugin removes the "grep" command from the output grepping through 'ps'
+THISDIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# I type this way too often to not make it a plugin
+# Tested on MacOs, Linux and Solaris
+function psgrep {
+  ps eax|grep $@|grep -v grep
+}
