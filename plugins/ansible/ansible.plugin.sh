4b3f222158736c065f857bcf5fe079c62542e952
Chris Blackburn <cblackburn@ajla.net>
2020-01-08 13:44:00 -0700

    Fix ansible-role-init

--- plugins/ansible/ansible.plugin.sh
+++ plugins/ansible/ansible.plugin.sh
@@ -1,5 +1,23 @@
 THISDIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+function treecmd {
+  if [[ -x $(which tree) ]]; then
+    tree $1
+  else
+    find $1
+  fi
+}
+
+function ansible-role-init {
+  if [[ ! -z $1 ]]; then
+    echo "Ansible Role: $1 Creating..."
+    ansible-galaxy init $1
+    treecmd $1
+  else
+    echo "Usage: ansible-role-init ROLE_NAME"
+  fi
+}
+
 # Ansible-related aliases
 alias acfg='ansible-config'
 alias acon='ansible-console'
@@ -13,3 +31,6 @@ alias apull='ansible-pull'
 alias arinit='ansible-role-init'
 alias av='ansible-vault'
 alias aver='ansible --version'
+
+# List the defined ansible aliases
+alias ansible_aliases='alias|grep ansible'
