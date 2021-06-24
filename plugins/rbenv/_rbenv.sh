999b322a9ff9e566240227eca69c6dad2ea587e1
Chris Blackburn <8dd969a2@opayq.com>
2021-06-24 10:05:43 -0600

    Add function to update ruby-build versions

--- plugins/rbenv/_rbenv.sh
+++ plugins/rbenv/_rbenv.sh
@@ -1,3 +1,11 @@
 pathmunge ~/.rbenv/bin before
 pathmunge ~/.rbenv/shims before
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+
+# This function is useful if you have installed ruby-build as a rbenv plugin using Git
+function rbenv-update-versions {
+  # Update the ruby-build plugin repo
+  gitdir="$(rbenv root)"/plugins/ruby-build/.git
+  worktree="$(rbenv root)"/plugins/ruby-build
+  git --git-dir="$gitdir" --work-tree="$worktree" pull
+}
