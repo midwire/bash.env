986290c5284be09f1bf76b39423d178f905e8b0e
Chris Blackburn <8dd969a2@opayq.com>
2021-11-11 20:08:56 -0700

    Init ssh-agent if not running

--- plugins/ssh/ssh.plugin.sh
+++ plugins/ssh/ssh.plugin.sh
@@ -2,6 +2,12 @@
 
 KEYS=`ls -1 ~/.ssh/id_* | grep -v ".pub"`
 
+# Initialize ssh-agent if not running
+pgrep -fx ssh-agent
+if [ $? -ne 0 ]; then
+  eval "$(ssh-agent)"
+fi
+
 for k in $KEYS ; do
   ssh-add "$k"
 done
